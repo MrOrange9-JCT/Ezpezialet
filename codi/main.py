cff6bf2b529a45ffeb7c234cb8c8239e669d61f3
MrOrange9-JCT <66780535+MrOrange9-JCT@users.noreply.github.com>
2020-06-28 20:14:29 +0200

    Update main.py

--- codi/main.py
+++ codi/main.py
@@ -274,7 +274,7 @@ async def minecraft(ctx, *, adress):
                 motd = data['motd']
 
                 embed = discord.Embed(title=f"{adress} ({name})", description=f"**Jugadors:** {players_now}/{players_max}\n \n{motd}\n \n**Previsualització Minecraft:**")
-		embed.set_image(f"http://status.mclive.eu/{adress}/{adress}/25565/banner.png")
+		embed.set_image(url=f"http://status.mclive.eu/{adress}/{adress}/25565/banner.png")
                 embed.set_footer(text="http://mcapi.us/")
 
                 await ctx.send(embed=embed)
